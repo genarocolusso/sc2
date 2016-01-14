6ba95ea61c05671ef1aa02e311e8e92c321a2d7f
Genaro Colusso <genarocolusso@gmail.com>
2016-01-14 09:57:06 -0200

    sc2 api call with angularJS

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var app = angular.module('starcraft', []);
+app.controller('myCtrl', function($scope, $http) {
+    var pendingTask; 
+     // https://us.api.battle.net/sc2/profile/602274/2/Colusso/ladders?locale=en_US&apikey=wn8zvrq35vgfqktdbypr4pfnvcdz9pcd
+  $scope.change = function() {
+      if (pendingTask) {
+        clearTimeout(pendingTask);
+      }
+      pendingTask = setTimeout(fetch, 800);
+    };
+
+    function fetch() {
+     $http.get("https://us.api.battle.net/sc2/profile/" + $scope.search + "/ladders?locale=en_US&apikey=wn8zvrq35vgfqktdbypr4pfnvcdz9pcd")
+        .success(function(response) {
+          $scope.details = response; 
+                  });
+     };
+
+});
